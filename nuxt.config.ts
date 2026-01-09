ad53ed0f1c319f54c3a72755477e0342d2246ae1
mmikolajczak <mmikolajczak@MMIKOLAJCZAK-MBP16.local>
2026-01-09 14:33:32 +0100

    nuxt config fix for vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,9 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
 	compatibilityDate: '2025-07-15',
+	nitro: {
+		preset: 'vercel',
+	},
 	vite: {
 		server: {
 			hmr: {
